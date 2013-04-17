e3c4753a2872faa825436be3322cd59841a695f6
Rushabh Padalia <padalia.rushabh@gmail.com>
2013-04-17 19:36:17 +0800

    add animal works

--- PHP/zoo/zoo.php
+++ PHP/zoo/zoo.php
@@ -1,12 +1,12 @@
 <?php
-include_once "../DatabaseMiddleLayer/DatabaseMiddleLayer.php"
+include_once "../DatabaseMiddleLayer/DatabaseMiddleLayer.php";
 
-public interface tView{
+interface tView{
 	
 	 public function view($where, $order);
 }  
 
-public interface tEdit{
+interface tEdit{
 	
 	public function update(array $data, $where);
 	public function add(array $data);
@@ -14,8 +14,7 @@ public interface tEdit{
 	
 	
 }
-
-public interface tMove{
+interface tMove{
 	
 	public function transfer($to);	
 }
@@ -25,17 +24,45 @@ public interface tMove{
 	
 } */
 
-public class animal implements tView, tEdit, tMove{
+class animal implements tView, tEdit, tMove{
 	
 	public function add(array $data){
 		
+		$connection = array("localhost","root","Rushabh","zoo");
+		$animal = new Mysql_DatabaseMiddleLayer($connection);
 		
+		$animal->insert('Cage_tb', $data);
 	}
+	
+	public function view($where, $order){}
+	public function update(array $data, $where){}
+	public function delete($where){}
+	public function transfer($to){}
+	
+	
 		
 }
 
-public class cage implements tview, tEdit{
+class cage implements tview, tEdit{
 	
+	public function view($where, $order){}
+	public function update(array $data, $where){}
+	public function delete($where){}
+	public function add(array $data){}
 }
+
+$an = new animal();
+$data = array(
+				'name'=>'Blackbuck'
+				,'size_height'=> 50
+				,'size_width'=>75
+				,'size_length'=>60
+				,'location_latitude'=>1.40495
+				,'location_longitude'=>103.79049
+				,'doors'=>2
+				,'animal_limit'=>2
+		
+			);
+$an->add($data);
 
 ?>
